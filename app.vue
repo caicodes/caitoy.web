67b70df76ffe17752cc3ea1ee551d57c5899a8be
caicodes <caicodes1@gmail.com>
2022-08-29 16:35:36 -0600

    animation polish, bg shadow, blur.

--- app.vue
+++ app.vue
@@ -3,20 +3,34 @@
     <AppHeader />
 
     <section class="content">
-      <div class="main-text">
+      <div class="main-text flex flex-col align-middle relative">
         <!-- <span class="whitespace-nowrap">Hi,</span><br />
           <span class="whitespace-nowrap">I'm Cai</span><br /> -->
-        <div class="create whitespace-nowrap">
-          I create<br />
-          <span class="experiences">experiences</span>
+
+        <div class="hello opacity-0 absolute">hello</div>
+        <div class="flex absolute">
+          <div class="im opacity-0">i'm</div>
+          <div class="cai opacity-0">cai</div>
         </div>
+
+        <div class="flex">
+          <div class="i opacity-0">i</div>
+          <div class="create opacity-0">create</div>
+        </div>
+
+        <span
+          @mouseleave="experiencesOff()"
+          @mouseover="experiencesOn()"
+          class="experiences opacity-0"
+          >experiences</span
+        >
       </div>
       <div class="img_group">
         <div class="img_group_inner">
-          <img src="./images/mobile/photo-1.jpg" alt="" />
-          <img src="./images/mobile/photo-2.jpg" alt="" />
-          <img src="./images/mobile/photo-3.jpg" alt="" />
           <img src="./images/mobile/photo-4.jpg" alt="" />
+          <img src="./images/mobile/photo-3.jpg" alt="" />
+          <img src="./images/mobile/photo-2.jpg" alt="" />
+          <img src="./images/mobile/photo-1.jpg" alt="" />
         </div>
       </div>
 
@@ -24,7 +38,7 @@
         class="bottom-bar fixed bottom-0 left-0 right-0 uppercase text-xs opacity-30 justify-between w-full"
       >
         <div class="flex justify-between p-5 gap-4">
-          <div>web developer / ui engineer / web producer</div>
+          <div>ui developer / frontend engineer / web producer</div>
           <div class="hidden md:block">
             instagram / twitter / youtube / github
           </div>
@@ -35,6 +49,94 @@
     <AppAnimatedBackground />
   </main>
 </template>
+
+<script setup>
+import { onMounted } from "vue"
+import gsap from "gsap"
+
+function experiencesOn() {
+  console.log("yo on it")
+  gsap.to(".img_group img", {
+    x: 100,
+    margin: "0 10px 0",
+    duration: 1,
+    opacity: 1,
+    rotate: 0,
+    stagger: 0.14,
+  })
+}
+function experiencesOff() {
+  console.log("yo off it")
+  gsap.to(".img_group img", {
+    x: 1100,
+    margin: "0 -140px 0",
+    duration: 1,
+    opacity: 1,
+    rotate: 15,
+    stagger: 0.14,
+  })
+}
+
+onMounted(() => {
+  const animation = gsap.timeline()
+
+  console.log("yo gsap")
+
+  animation
+    // .fromTo(
+    //   ".hello",
+    //   { opacity: 0, x: -40 },
+    //   { opacity: 1, x: 0, delay: 1, duration: 0.5, ease: "power4.easeOut" }
+    // )
+    // .to(".hello", { delay: 1, opacity: 0, display: "none" })
+    // .fromTo(
+    //   ".im",
+    //   { opacity: 0, x: -40 },
+    //   { opacity: 1, x: 0, delay: 0.5, duration: 0.5, ease: "power4.easeOut" }
+    // )
+
+    // .fromTo(
+    //   ".cai",
+    //   { opacity: 0, x: -40 },
+    //   { opacity: 1, x: 0, delay: 0, duration: 0.5, ease: "power4.easeOut" }
+    // )
+    // .to(".im,.cai", { delay: 2, opacity: 0, stagger: 0.25, ease: "easeIn" })
+
+    // .fromTo(
+    //   ".i",
+    //   { opacity: 0, x: -40 },
+    //   { opacity: 1, x: 0, delay: 0.5, duration: 0.35, ease: "power4.easeOut" }
+    // )
+    // .fromTo(
+    //   ".create",
+    //   { opacity: 0, x: -40 },
+    //   { opacity: 1, x: 0, delay: 0, duration: 1.25, ease: "power4.easeOut" }
+    // )
+    .fromTo(
+      ".experiences",
+      { opacity: 0, x: -40 },
+      { opacity: 1, x: 0, delay: 0.25, duration: 3, ease: "power4.easeOut" }
+    )
+    .from(".img_group img", {
+      x: 1100,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.14,
+    })
+    .to(
+      ".img_group img",
+      {
+        x: 1100,
+        margin: "0 -140px 0",
+        duration: 1,
+        opacity: 1,
+        rotate: 15,
+        stagger: 0.14,
+      },
+      "-=1.4"
+    )
+})
+</script>
 
 <style>
 .content {
@@ -53,7 +155,6 @@
 }
 .img_group_inner {
   display: flex;
-  @apply gap-8 md:gap-16;
 }
 .content .img_group {
   position: absolute;
@@ -71,9 +172,9 @@
 }
 
 .main-text {
-  font-size: 8vw;
+  font-size: 12vw;
   font-weight: 400;
-  line-height: 8vw;
+  line-height: 10vw;
   text-transform: uppercase;
   text-align: left;
   cursor: default;
@@ -84,7 +185,7 @@
   display: flex;
   width: auto;
 }
-.experiences:before {
+.experiences:after {
   content: "";
   position: absolute;
   bottom: 0;
@@ -93,7 +194,45 @@
   background-color: white;
   transition: all ease 0.5s;
 }
-.experiences:hover:before {
+.experiences:hover:after,
+.experiences:hover::before {
   width: 100%;
+}
+.experiences:hover::before {
+  height: 50vh;
+}
+.experiences:before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  background-image: linear-gradient(to top, rgba(0 0 0/60%), transparent);
+  transition: all ease 0.5s;
+  z-index: -40;
+  @apply blur-2xl;
+}
+
+.content .img_group :before {
+  content: "";
+  z-index: 20;
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(to top, rgba(0 0 0/70%), transparent);
+  opacity: 0;
+}
+.content .img_group :after {
+  content: "";
+  z-index: 20;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(to bottom, rgba(0 0 0/70%), transparent);
+  opacity: 0;
 }
 </style>
