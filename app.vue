8b4a7ec5c103cf295d8c7644dcf8cd15c949a46b
caicodes <caicodes1@gmail.com>
2022-08-28 21:51:31 -0600

    resetting to the base

--- app.vue
+++ app.vue
@@ -1,13 +1,3 @@
 <template>
   <AnimatedBackground />
-  <Navbar />
-  <Hero />
-  <AcclaimedMovies />
-  <DiscoverMore />
-  <FindYourNext />
-  <StreamAnytime />
-  <FavoriteDevices />
-  <WaysToGet />
-  <Questions />
-  <Footer />
 </template>
