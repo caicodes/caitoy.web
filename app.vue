c9cfebdf66430a287cfaac344fdbdcbe43a58649
caicodes <caicodes1@gmail.com>
2022-09-02 16:37:12 -0600

    adjusted firebase init to be an import, log events can be used within specific components without code duplication

--- app.vue
+++ app.vue
@@ -9,23 +9,12 @@
   </main>
 </template>
 
-
 <script setup>
-import { initializeApp } from "firebase/app";
+import firebase from './firebase/firebase'
 import { getAnalytics, logEvent } from "firebase/analytics";
-const firebaseConfig = {
-  apiKey: "AIzaSyDF4490c7nCn8G16n_FlaAWAiv0_1esOLE",
-  authDomain: "cai-toy.firebaseapp.com",
-  projectId: "cai-toy",
-  storageBucket: "cai-toy.appspot.com",
-  messagingSenderId: "742076604482",
-  appId: "1:742076604482:web:26bc2775138c1ee6e28ddb",
-  measurementId: "G-6T0GKYLR39"
-};
-const app = initializeApp(firebaseConfig);
 onMounted(() => {
-  const analytics = getAnalytics(app);
-  logEvent(analytics, 'portfolio_app_view');
+  const analytics = getAnalytics(firebase)
+  logEvent(analytics, "portfolio_app_view")
 })
 </script>
 
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAnalytics, logEvent } from "firebase/analytics"
 const firebaseConfig = {
   apiKey: "AIzaSyDF4490c7nCn8G16n_FlaAWAiv0_1esOLE",
   authDomain: "cai-toy.firebaseapp.com",
@@ -15,7 +9,6 @@ const firebaseConfig = {
   appId: "1:742076604482:web:26bc2775138c1ee6e28ddb",
   measurementId: "G-6T0GKYLR39",
 }
+const firebase = initializeApp(firebaseConfig)
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+export default firebase
